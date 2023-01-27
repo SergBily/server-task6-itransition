f8e26cdd6db5e1249f13e1c4b8eb9eabd99270a5
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-27 19:51:41 +0300

    feat: add user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,8 @@
 import { Schema, model } from 'mongoose';
 
 const UserSchema = new Schema({
-  name: {type: String, require: true, uniqe: true}
+  name: { type: String, require: true, uniqe: true },
+  status: {type: Boolean, default: false}
 })
 
 const UserModel = model('User', UserSchema);
